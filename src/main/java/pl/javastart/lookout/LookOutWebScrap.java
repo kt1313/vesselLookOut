db34a1338532f857b3417e8bd4f1627f660c71dd
kt1313 <tomaklim@wp.pl>
2021-02-23 15:17:59 +0100

    wprowadzanie index.jsp

--- src/main/java/pl/javastart/lookout/LookOutWebScrap.java
+++ src/main/java/pl/javastart/lookout/LookOutWebScrap.java
@@ -1,4 +1,24 @@
 package pl.javastart.lookout;
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import java.io.IOException;
+
 
 public class LookOutWebScrap {
+
+
+
+    public class JsoupTest {
+        public  void main(String[] args) throws IOException {
+            Connection connect = Jsoup.connect("http://www.onet.pl/");
+            Document document = connect.get();
+            Elements allH1 = document.select("h1");
+            for(Element elem: allH1) {
+                System.out.println(elem.text());
+            }
+        }
+    }
 }
