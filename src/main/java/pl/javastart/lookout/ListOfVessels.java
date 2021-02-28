f1458e4f603f4b5ee68b83b8ba9ef437ecdc1c3a
kt1313 <tomaklim@wp.pl>
2021-02-28 03:08:36 +0100

    wyszukiwanie listy statkow działa. tworzy dwie listy: nazw i IMO

--- src/main/java/pl/javastart/lookout/ListOfVessels.java
+++ src/main/java/pl/javastart/lookout/ListOfVessels.java
@@ -14,31 +14,28 @@ import java.util.List;
 
 class ListOfVessels {
     public static void main(String[] args) throws IOException {
-
-        String vslName = "Dolphin";
+        String vslName = "Telford";
         Connection connect = Jsoup.connect("https://www.shiplocation.com/vessels?page=1&vessel=" + vslName + "&sort=none&direction=none&flag=none");
         Document document = connect.get();
         Elements vElements = document.getElementsByClass("vessel_td");
-        List<String> listOfVessels = new ArrayList<>();
-        List<String> listOfVessels2 = new ArrayList<>();
+        List<String> listOfVesselsNames = new ArrayList<String>();
+        List<String> listOfVesselsIMOs = new ArrayList<>();
         System.out.println("");
-        System.out.println("Szukasz statku: "+vslName);
+        System.out.println("Szukasz statku: " + vslName);
         System.out.println("*******************************");
         System.out.println("Znalazłeś: ");
         for (Element e : vElements
         ) {
-            //sprawdz czy element zawiera w ownText()
-            // nazwe szukanego statku
-            String testString = vslName.substring(0, vslName.length());
+            //sprawdz czy element nie jest pusty i dodaj do listOfVessels
+            //znajdz IMO nr, polacz z nazwa statku i dodaj do listy
             if (!e.select("a").text().equals("")) {
-                listOfVessels.add(e.select("a").text());
+//                String nameAndIMOString=e.select("a").text()+" IMO nr: "+e.parent().getElementsByClass("vessel_td td_imo").text();
+                listOfVesselsNames.add(e.select("a").text());
+                listOfVesselsIMOs.add(e.parent().getElementsByClass("vessel_td td_imo").text());
             }
         }
-        int i=1;
-        for (String s : listOfVessels
-        ) {
-            if (!s.equals("")) {
-                System.out.println(i+":"+s); i++;           }
+        for (int i = 0; i < listOfVesselsNames.size(); i++) {
+            System.out.println(i + 1 + ": " + listOfVesselsNames.get(i) + " nr IMO: "+listOfVesselsIMOs.get(i));
         }
         System.out.println("*******************************");
         System.out.println("Wybierz nr.");
